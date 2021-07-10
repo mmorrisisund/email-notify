f09e12115d21274e53b86bda84332bc9fda3a0d5
Mike Morris <michael.a.morris.public@gmail.com>
2021-07-10 11:17:15 -0500

    refactor: move app component Move app to within component folder

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import './index.css'
+import App from './components/app'
 
 ReactDOM.render(
-  <React.StrictMode>{/* <App /> */}</React.StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 )
