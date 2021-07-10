9d616da506f729e961b8d5b957cd1d02314d9297
Mike Morris <michael.a.morris.public@gmail.com>
2021-07-10 14:11:44 -0500

    feat: add footer component

---
+++ src/components/Layout/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className='h-10 bg-blue-400 p-2 text-center'>
+      <span className='text-blue-50'>
+        Copyright Principal Financial Group &copy; {new Date().getFullYear()}
+      </span>
+    </footer>
+  )
+}
+
+export default Footer
